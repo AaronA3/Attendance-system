c4992109b62648b01b79035cf4a46bc12ed233f9
AaronA3 <75817389+AaronA3@users.noreply.github.com>
2021-01-10 11:27:38 -0800

    App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../logo.svg';
+import POG from './POG.js';
+import layout from './layout.js';
+
+
+function App() {
+  return (
+    
+    <header>
+      <layout />
+    </header>
+     
+  );
+}
+
+
+export default App;
